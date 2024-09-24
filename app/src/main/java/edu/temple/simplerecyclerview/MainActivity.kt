81b7ea444e3ba4fc5ed697a08af9f1ffa70baf08
John Joseph II <jackjoseph7447@gmail.com>
2024-09-24 12:14:18 -0400

    completed

--- app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
@@ -2,6 +2,7 @@ package edu.temple.simplerecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -9,12 +10,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // TODO Step 1: Reference RecyclerView object
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
 
-        //TODO Step 2: Provide a LayoutManager
-
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
         //TODO Step 4: Provide a RecyclerView.Adapter
+        val array = IntArray(10)
+        array[0] = 10
+        array[1] = 12
+        array[2] = 14
+        array[3] = 16
+        array[4] = 18
+        array[5] = 20
+        array[6] = 18
+        array[7] = 16
+        array[8] = 14
+        array[9] = 12
+
+        recyclerView.adapter = NumberDisplayAdapter(array)
     }
 }
